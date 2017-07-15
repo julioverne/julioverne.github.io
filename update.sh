6a79d5586d7e242522e78114d18715c56c154589
julioverne <jlio_verne@hotmail.com>
2017-07-15 17:20:54 -0300

    .

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 apt-ftparchive packages ./ > ./Packages
+sed -i -e '/^SHA/d' ./Packages
 bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: julioverne's Repo\nLabel: julioverne\nSuite: stable\nVersion: 1.0\nCodename: julioverne\nArchitecture: iphoneos-arm\nComponents: main\nDescription: julioverne's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" Packages.bz2\n" >Release;
+printf "Origin: julioverne's Repo\nLabel: julioverne\nSuite: stable\nVersion: 1.0\nCodename: julioverne\nArchitecture: iphoneos-arm\nComponents: main\nDescription: julioverne's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 exit 0
